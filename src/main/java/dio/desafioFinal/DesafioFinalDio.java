b3383a73e78974e192457f8d4bfdfe1b4a2a3309
Cedric <cedricbatalha@gmail.com>
2024-07-28 09:27:06 -0300

    criando o projeto maven do Desafio Final

---
+++ src/main/java/dio/desafioFinal/DesafioFinalDio.java
@@ -0,0 +1,13 @@
+package dio.desafioFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioFinalDio {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioFinalDio.class, args);
+	}
+
+}
---
+++ src/test/java/dio/desafioFinal/DesafioFinalDio.java
@@ -0,0 +1,13 @@
+package dio.desafioFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioFinalDio {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
